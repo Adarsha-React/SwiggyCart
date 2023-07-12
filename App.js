bbddb24be34b91e2d25fb9b34fab120fa0fba2ce
Adarsha-React <adarshareact2023@gmail.com>
2023-07-12 21:05:14 +0530

    Basic Layout - First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/Header";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <h1>App is working</h1>
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
